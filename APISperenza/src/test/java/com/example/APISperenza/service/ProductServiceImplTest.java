9180f3df9c283a3678ac757db0c5cb65149f92e1
Honor <55959605+HonoR974@users.noreply.github.com>
2022-10-31 13:30:23 +0100

    test
    debut des test

--- APISperenza/src/test/java/com/example/APISperenza/service/ProductServiceImplTest.java
+++ APISperenza/src/test/java/com/example/APISperenza/service/ProductServiceImplTest.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.InjectMocks;
 import static org.mockito.Mockito.*;
@@ -16,6 +17,7 @@ import static org.mockito.MockitoAnnotations.initMocks;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.example.APISperenza.exception.ResourceNotFoundException;
 import com.example.APISperenza.model.Product;
 import com.example.APISperenza.repository.ProductRepository;
 
@@ -33,6 +35,13 @@ public class ProductServiceImplTest {
     @Before
     public void setUp()
     {
+        initMocks(this);
+    }
+
+    @Test
+    public void getAllProduct()
+    {
+        
         Product product = new Product();
         Product product2 = new Product();
 
@@ -41,18 +50,26 @@ public class ProductServiceImplTest {
         list.add(product);
         list.add(product2);
 
-        initMocks(this);
-
         when(productRepository.findAll()).thenReturn(list);
+
+
+        List<Product> list2 = productServiceImpl.getAll();
+
+        assertThat(list2.size()).isEqualTo(2);
     }
 
     @Test
-    public void getAllProduct()
+    public void getByIdProduct()
     {
-        List<Product> list = productServiceImpl.getAll();
+        Product product4 = new Product();
+        product4.setName("sac");
 
-        assertThat(list.size()).isEqualTo(2);
+        when(productRepository.findById(4).orElseThrow( () -> new ResourceNotFoundException("ce produit n'exte pas "))).thenReturn(product4);
+        Product product = productServiceImpl.getByIdProduct(4);
+
+        
+        Mockito.verify(productRepository.findById(4));
+        assertThat(product.getName()).isEqualTo(product4.getName());
     }
-
 
 }
